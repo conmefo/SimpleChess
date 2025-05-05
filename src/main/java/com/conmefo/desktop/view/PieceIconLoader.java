38c4c42272d9c1de3914969240db6c7d9a447505
conmefo <hoquangminh757@gmail.com>
2025-05-05 23:17:28 +0700

    images

---
+++ src/main/java/com/conmefo/desktop/view/PieceIconLoader.java
@@ -0,0 +1,13 @@
+package com.conmefo.desktop.view;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
+public class PieceIconLoader {
+    private static final Map<String, ImageIcon> cache = new HashMap<>(
+
+    );
+
+}
